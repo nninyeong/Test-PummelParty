ab54032804f1e4031f5a1b2f088bd6828d8a0a07
rickythediver <wooseok1995@gmail.com>
2023-05-21 21:14:05 +0900

    [Feature] Added Statistics related scripts
    Added Statistics related Scripts
    1) StatisticsData
    2) StatisticsCalculator

---
+++ Prototype-Pummel Party/Assets/Scripts/Statistics/StatisticsCalculator.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatisticsCalculator : MonoBehaviour
+{
+    // StatisticsData 클래스의 데이터를 가지고 계산을 담당하는 클래스
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Prototype-Pummel Party/Assets/Scripts/Statistics/StatisticsData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatisticsData
+{
+    // 통계 관련 데이터만 갖고 있는 클래스
+
+
+}
